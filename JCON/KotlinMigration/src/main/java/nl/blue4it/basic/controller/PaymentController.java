270d0e77c0193a5379fb12f3602fe1797a69a56b
Turk <ko.turk@rabobank.nl>
2022-09-18 17:37:30 +0200

    added validation on domain object

--- JCON/KotlinMigration/src/main/java/nl/blue4it/basic/controller/PaymentController.java
+++ JCON/KotlinMigration/src/main/java/nl/blue4it/basic/controller/PaymentController.java
@@ -7,23 +7,26 @@ import nl.blue4it.basic.service.PaymentService;
 import nl.blue4it.basic.service.exception.PaymentRejectedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Validated
 public class PaymentController {
     private final PaymentService paymentService;
     @PostMapping
-    public ResponseEntity<String> processPayment(@RequestBody List<Payment> payments) throws PaymentRejectedException {
+    public ResponseEntity<String> processPayment(@Valid @RequestBody List<Payment> payment) throws PaymentRejectedException {
         log.info("Processing payments");
 
         try {
-            paymentService.processPayment(payments);
+            paymentService.processPayment(payment);
             log.info("Payment processed");
             return ResponseEntity.ok("Created payment");
         } catch (PaymentRejectedException ex) {
--- JCON/KotlinMigration/src/main/java/nl/blue4it/basic/controller/dto/request/Payment.java
+++ JCON/KotlinMigration/src/main/java/nl/blue4it/basic/controller/dto/request/Payment.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 // 1. convert to kotlin
 // 2. change to data class
 // 3. show null value
@@ -12,10 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Payment {
 
+    @NotNull
     private String name;
+    @NotNull
     private String fromIban;
+    @NotNull
     private String toIban;
+    @NotNull
     private Float amount;
+    @NotNull
     private Float balance;
 
     // could be null, otherwise it could be weekly, monthly or yearly
